697c131dc631eef1bd4dcce928750e014e1c8119
David Smith <dsmith@redhat.com>
2018-01-04 15:28:47 -0600

    Add a port option to the web server.
    * httpd/main.cxx (parse_cmdline): New function.
      (main): Call parse_cmdline() to handle the the new '--port' option.

--- httpd/main.cxx
+++ httpd/main.cxx
@@ -9,12 +9,14 @@
 #include "server.h"
 #include "api.h"
 #include <iostream>
+#include "../util.h"
 
 extern "C" {
 #include <string.h>
 #include <signal.h>
 #include <errno.h>
 #include <sys/signalfd.h>
+#include <getopt.h>
 }
 
 server *httpd = NULL;
@@ -88,15 +90,63 @@ setup_main_signals(pthread_t *tid)
     }
 }
 
+// FIXME: the default port of 1234 was just chosen at random. A better
+// port default needs to be chosen.
+static uint16_t port = 1234;
+
+void
+parse_cmdline(int argc, char *const argv[])
+{
+    enum {
+	LONG_OPT_PORT = 256,
+    };
+    static struct option long_options[] = {
+        { "port", 1, NULL, LONG_OPT_PORT },
+        { NULL, 0, NULL, 0 }
+    };
+    while (true) {
+	int grc = getopt_long(argc, argv, "", long_options, NULL);
+	char *num_endptr;
+	unsigned long port_tmp;
+	if (grc < 0)
+	    break;
+	switch (grc) {
+	  case LONG_OPT_PORT:
+	    errno = 0;
+	    port_tmp = strtoul(optarg, &num_endptr, 10);
+	    if (*num_endptr != '\0') {
+		cerr << (_F("%s: cannot parse number '--port=%s'", argv[0],
+			    optarg)) << endl;
+		exit(1);
+	    }
+	    if (errno != 0 || port_tmp > 65535) {
+		cerr << (_F("%s: invalid entry: port must be between 0 and 65535 '--port=%s'",
+			    argv[0], optarg)) << endl;
+		exit(1);
+	    }
+	    port = (uint16_t)port_tmp;
+	    break;
+	  default:
+	    break;
+	}
+    }
+    for (int i = optind; i < argc; i++) {
+	cerr << (_F("%s: unrecognized argument '%s'", argv[0], argv[i]))
+	     << endl;
+    }
+}
+
 int
-main(int /*argc*/, char *const /*argv*/[])
+main(int argc, char *const argv[])
 {
     pthread_t tid;
 
     setup_main_signals(&tid);
 
+    parse_cmdline(argc, argv);
+
     // Create the server and ask the api to register its handlers.
-    httpd = new server(1234);
+    httpd = new server(port);
     api_add_request_handlers(*httpd);
 
     // Wait for the server to shut itself down.
