danielpoliakov/systemtap

commit eb3896ef09b939ceb04d6c678250be52ba1a401a

Add the capability to list http servers.
* client-http.cxx (fill_in_server_info): New function.
* client-http.h (client_backend): Added 'fill_in_server_info' prototype.
* client-nss.h (client_backend): Added 'fill_in_server_info' stub.
* csclient.cxx (nss_get_client_backend): New function.
  (compile_server_client::passes_0_4): Calls new 'nss_get_client_backend'
  function.
* csclient.h: Added client_backend::fill_in_server_info() virtual
  function and 'nss_get_client_backend' prototype.
* httpd/Makefile.am (stap_httpd_SOURCES): Now includes ../cscommon.cxx.
* httpd/Makefile.in: Regenerated.
* httpd/nss_funcs.cxx (nss_get_server_cert_info): New function.
* httpd/nss_funcs.h: Added 'nss_get_server_cert_info' prototype.
* httpd/server.cxx (base_dir_rh::GET): When a GET is done on the server
  root directory, the server architecture and certificate info is
  returned.
* nss-server-info.cxx (compile_server_info::host_specification): New
  function.
  (query_server_status): Add support for http servers.
  (nss_get_specified_server_info): Ditto.
  (nss_get_or_keep_compatible_server_info): A server can be compatible if
  the architectures match.
  (nss_add_online_server_info): New function.
* nss-server-info.h: Added compile_server_info::host_specification() and
  nss_add_online_server_info() prototypes.

nss-server-info.cxx
@@ -110,6 +110,42 @@ compile_server_info::isComplete () const
   return this->hasAddress () && port != 0;
 }
 
+string 
+compile_server_info::host_specification () const
+{
+  ostringstream host_spec;
+  if (!this->isComplete())
+      return host_spec.str();
+
+  if (! this->host_name.empty ())
+      host_spec << this->host_name;
+  else if (this->hasAddress())
+    {
+      PRStatus prStatus;
+      switch (this->address.raw.family)
+	{
+	case PR_AF_INET:
+	case PR_AF_INET6:
+	  {
+#define MAX_NETADDR_SIZE 46 // from the NSPR API reference.
+	    char buf[MAX_NETADDR_SIZE];
+	    prStatus = PR_NetAddrToString(&this->address, buf, sizeof (buf));
+	    if (prStatus == PR_SUCCESS) {
+	      host_spec << buf;
+	      break;
+	    }
+	  }
+	  break;
+	default:
+	  // Do nothing.
+	  break;
+	}
+    }
+  if (! host_spec.str().empty())
+      host_spec << ":" << this->port;
+  return host_spec.str();
+}
+
 bool
 compile_server_info::operator== (const compile_server_info &that) const
 {
@@ -983,13 +1019,18 @@ query_server_status (systemtap_session &s, const string &status_string)
     working_string = "specified";
 
   // If the query is "specified" and no servers have been specified
-  // (i.e. --use-server not used or used with no argument), then
-  // use the default query.
+  // (i.e. '--use-server' and/or '--use-http-server' not used or used
+  // with no argument), then use the default query.
+  //
   // TODO: This may not be necessary. The underlying queries should handle
   //       "specified" properly.
-  if (working_string == "specified" &&
-      (s.specified_servers.empty () ||
-       (s.specified_servers.size () == 1 && s.specified_servers[0].empty ())))
+  if (working_string == "specified"
+      && (s.specified_servers.empty ()
+	  || (s.specified_servers.size () == 1
+	      && s.specified_servers[0].empty ()))
+      && (s.http_servers.empty()
+	  || (s.http_servers.size () == 1
+	      && s.http_servers[0].empty ())))
     working_string = default_server_spec (s);
 
   int pmask = server_spec_to_pmask (working_string);
@@ -1536,7 +1577,7 @@ nss_get_specified_server_info (
 
       // If --use-server was not specified at all, then return info for the
       // default server list.
-      if (s.specified_servers.empty ())
+      if (s.specified_servers.empty () && s.http_servers.empty ())
 	{
 	  if (s.verbose >= 3)
 	    clog << _("No servers specified") << endl;
@@ -1545,14 +1586,21 @@ nss_get_specified_server_info (
 	}
       else
 	{
-	  // Iterate over the specified servers. For each specification, add to
-	  // the list of servers.
-	  unsigned num_specified_servers = s.specified_servers.size ();
-	  for (unsigned i = 0; i < num_specified_servers; ++i)
-	    {
-	      string &server = s.specified_servers[i];
+	  // Iterate over the specified servers. For each
+	  // specification, add to the list of servers.
+	  vector<string> &server_list = (! s.http_servers.empty ()
+					 ? s.http_servers
+					 : s.specified_servers);
+	  client_backend *backend = nss_get_client_backend (s);
+	  if (backend->initialize () != 0)
+	    return;
 
-	      // If no specific server(s) specified, then use the default servers.
+	  for (auto it = server_list.begin (); it != server_list.end (); ++it)
+	    {
+	      string &server = *it;
+
+	      // If no specific server(s) specified, then use the
+	      // default servers.
 	      if (server.empty ())
 		{
 		  if (s.verbose >= 3)
@@ -1562,18 +1610,21 @@ nss_get_specified_server_info (
 		  continue;
 		}
 
-	      // Determine what has been specified. Servers may be specified by:
+	      // Determine what has been specified. Servers may be
+	      // specified by:
 	      // - domain{:port}
 	      // - certificate-serial-number{:port}
               // - IPv4-address{:port}
               // - IPv6-address{:port}
 	      // where items within {} are optional.
-	      // Check for IPv6 addresses first. It reduces the amount of checking necessary for
-	      // certificate serial numbers.
+	      //
+	      // Check for IPv6 addresses first. It reduces the amount
+	      // of checking necessary for certificate serial numbers.
 	      compile_server_info server_info;
 	      vector<compile_server_info> resolved_servers;
-	      if (isIPv6 (server, server_info) || isIPv4 (server, server_info) ||
-		  isCertSerialNumber (server, server_info))
+	      if (isIPv6 (server, server_info)
+		  || isIPv4 (server, server_info)
+		  || isCertSerialNumber (server, server_info))
 		{
 		  // An address or cert serial number has been specified.
 		  // No resolution is needed.
@@ -1586,22 +1637,31 @@ nss_get_specified_server_info (
 		}
 	      else
 		{
-		  clog << _F("Invalid server specification for --use-server: %s", server.c_str())
+		  clog << _F("Invalid server specification: %s", server.c_str())
 		       << endl;
 		  continue;
 		}
 
-	      // Now examine the server(s) identified and add them to the list of specified
-	      // servers.
+	      // Now examine the server(s) identified and add them to
+	      // the list of specified servers.
 	      vector<compile_server_info> known_servers;
 	      vector<compile_server_info> new_servers;
-	      for (vector<compile_server_info>::iterator i = resolved_servers.begin();
+	      for (auto i = resolved_servers.begin();
 		   i != resolved_servers.end();
 		   ++i)
 		{
 		  // If this item was fully specified, then just add it.
-		  if (i->fully_specified)
+		  if (i->fully_specified) {
+		    // In this instance, "fully specified" means
+		    // address and port. At this point we haven't
+		    // tried to contact the server to get online
+		    // information, certificate information,
+		    // etc. Certain server types need us to connect to
+		    // the server directly to get this information.
+		    if (backend)
+		      backend->fill_in_server_info (*i);
 		    nss_add_server_info (*i, new_servers);
+		  }
 		  else {
 		    // It was not fully specified, so we need additional info. Try
 		    // to get it by matching what we have against other known servers.
@@ -1777,7 +1837,8 @@ nss_get_or_keep_compatible_server_info (
       assert (! servers[i].empty ());
 
       // Check the target of the server.
-      if (servers[i].sysinfo != s.kernel_release + " " + s.architecture)
+      if (servers[i].sysinfo != s.kernel_release + " " + s.architecture
+	  || servers[i].sysinfo != s.architecture)
 	{
 	  // Target platform mismatch.
 	  servers.erase (servers.begin () + i);
@@ -2444,6 +2505,12 @@ merge_server_info (
     target.certinfo = source.certinfo;
 }
 
+void
+nss_add_online_server_info (systemtap_session &s,
+			    const compile_server_info &info)
+{
+  nss_add_server_info (info, cscache(s)->online_servers);
+}
 #endif // HAVE_NSS
 
 /* vim: set sw=2 ts=8 cino=>4,n-2,{2,^-2,t0,(0,u0,w1,M1 : */

client-http.cxx
@@ -1121,4 +1121,40 @@ http_client_backend::add_mok_fingerprint (const std::string &)
   return;
 }
 
+void
+http_client_backend::fill_in_server_info (compile_server_info &info)
+{
+  // Try to connect to the server. We'll try to grab the base
+  // directory of the server just to see if we can make a
+  // connection.
+  string host_spec = info.host_specification ();
+  if (host_spec.empty())
+    return;
+
+  string url = host_spec + "/";
+  if (http->download (url, http->json_type))
+    {
+      json_object *ver_obj;
+      json_bool jfound;
+
+      // Get the server version number.
+      jfound = json_object_object_get_ex (http->root, "version", &ver_obj);
+      if (jfound)
+	info.version = json_object_get_string(ver_obj);
+
+      // Get the server arch.
+      jfound = json_object_object_get_ex (http->root, "arch", &ver_obj);
+      if (jfound)
+	info.sysinfo = json_object_get_string(ver_obj);
+
+      // Get the server certificate info.
+      jfound = json_object_object_get_ex (http->root, "cert_info", &ver_obj);
+      if (jfound)
+	info.certinfo = json_object_get_string(ver_obj);
+
+      // If the download worked, this server is obviously online.
+      nss_add_online_server_info (s, info);
+  }
+}
+
 #endif /* HAVE_HTTP_SUPPORT */

client-http.h
@@ -43,6 +43,7 @@ public:
   void add_mok_fingerprint(const std::string &fingerprint);
   int finalize_mok_fingerprints() { return 0; };
 
+  void fill_in_server_info (compile_server_info &info);
 
   // FIXME: The 'request_parameters' data item isn't right. This means
   // we can only add string parameters, not numeric parameters. We

client-nss.h
@@ -42,6 +42,8 @@ public:
   void add_mok_fingerprint(const std::string &fingerprint);
   int finalize_mok_fingerprints();
 
+  void fill_in_server_info (compile_server_info &) { return; };
+
   int package_request ();
   int find_and_connect_to_server ();
   int unpack_response ();

csclient.cxx
@@ -14,7 +14,9 @@
 #include "cscommon.h"
 #include "csclient.h"
 #include "client-nss.h"
+#ifdef HAVE_HTTP_SUPPORT
 #include "client-http.h"
+#endif
 #include "util.h"
 #include "stap-probe.h"
 
@@ -29,18 +31,25 @@ extern "C" {
 
 using namespace std;
 
-int
-compile_server_client::passes_0_4 ()
+client_backend *
+nss_get_client_backend (systemtap_session &s)
 {
   // Use the correct backend.
 #ifdef HAVE_HTTP_SUPPORT
   if (! s.http_servers.empty())
-      backend = new http_client_backend (s);
+    return new http_client_backend (s);
 #endif
 #if HAVE_NSS
-  if (backend == NULL)
-      backend = new nss_client_backend (s);
+  return new nss_client_backend (s);
 #endif
+  return NULL;
+}
+
+int
+compile_server_client::passes_0_4 ()
+{
+  // Use the correct backend.
+  backend = nss_get_client_backend (s);
   if (backend == NULL)
     {
       clog << _("Using a compile server backend failed.") << endl;

csclient.h
@@ -9,7 +9,8 @@
 #define CSCLIENT_H
 
 #include "cscommon.h"
-#include "string"
+#include "nss-server-info.h"
+#include <string>
 
 class client_backend
 {
@@ -36,6 +37,8 @@ public:
 
   virtual void add_mok_fingerprint(const std::string &fingerprint) = 0;
   virtual int finalize_mok_fingerprints() = 0;
+
+  virtual void fill_in_server_info (compile_server_info &info) = 0;
 
   std::string server_tmpdir;
   cs_protocol_version server_version;
@@ -68,5 +71,8 @@ private:
 
   std::string client_tmpdir;
 };
+
+// Return the appropriate client backend for the current session.
+client_backend *nss_get_client_backend (systemtap_session &s);
 
 #endif // CSCLIENT_H

httpd/Makefile.am
@@ -18,7 +18,7 @@ AM_LDFLAGS = @PIELDFLAGS@
 if HAVE_HTTP_SUPPORT
 pkglibexec_PROGRAMS = stap-httpd
 
-stap_httpd_SOURCES = main.cxx server.cxx api.cxx backends.cxx utils.cxx nss_funcs.cxx ../util.cxx ../cmdline.cxx ../nsscommon.cxx ../privilege.cxx
+stap_httpd_SOURCES = main.cxx server.cxx api.cxx backends.cxx utils.cxx nss_funcs.cxx ../util.cxx ../cmdline.cxx ../nsscommon.cxx ../privilege.cxx ../cscommon.cxx
 stap_httpd_CFLAGS = $(AM_CFLAGS) $(nss_CFLAGS)
 stap_httpd_CXXFLAGS = $(AM_CXXFLAGS) $(nss_CFLAGS)
 stap_httpd_CPPFLAGS = $(AM_CPPFLAGS) $(nss_CFLAGS)

httpd/Makefile.in
@@ -122,7 +122,8 @@ am__dirstamp = $(am__leading_dot)dirstamp
 @HAVE_HTTP_SUPPORT_TRUE@	../stap_httpd-util.$(OBJEXT) \
 @HAVE_HTTP_SUPPORT_TRUE@	../stap_httpd-cmdline.$(OBJEXT) \
 @HAVE_HTTP_SUPPORT_TRUE@	../stap_httpd-nsscommon.$(OBJEXT) \
-@HAVE_HTTP_SUPPORT_TRUE@	../stap_httpd-privilege.$(OBJEXT)
+@HAVE_HTTP_SUPPORT_TRUE@	../stap_httpd-privilege.$(OBJEXT) \
+@HAVE_HTTP_SUPPORT_TRUE@	../stap_httpd-cscommon.$(OBJEXT)
 stap_httpd_OBJECTS = $(am_stap_httpd_OBJECTS)
 am__DEPENDENCIES_1 =
 @HAVE_HTTP_SUPPORT_TRUE@stap_httpd_DEPENDENCIES =  \
@@ -413,7 +414,7 @@ AM_CPPFLAGS = -D_GNU_SOURCE -I$(srcdir)/../includes \
 	-DSYSCONFDIR='"$(sysconfdir)"' -DPKGDATADIR='"${pkgdatadir}"' \
 	-DPKGLIBDIR='"$(pkglibexecdir)"' -DLOCALEDIR='"$(localedir)"'
 AM_LDFLAGS = @PIELDFLAGS@
-@HAVE_HTTP_SUPPORT_TRUE@stap_httpd_SOURCES = main.cxx server.cxx api.cxx backends.cxx utils.cxx nss_funcs.cxx ../util.cxx ../cmdline.cxx ../nsscommon.cxx ../privilege.cxx
+@HAVE_HTTP_SUPPORT_TRUE@stap_httpd_SOURCES = main.cxx server.cxx api.cxx backends.cxx utils.cxx nss_funcs.cxx ../util.cxx ../cmdline.cxx ../nsscommon.cxx ../privilege.cxx ../cscommon.cxx
 @HAVE_HTTP_SUPPORT_TRUE@stap_httpd_CFLAGS = $(AM_CFLAGS) $(nss_CFLAGS)
 @HAVE_HTTP_SUPPORT_TRUE@stap_httpd_CXXFLAGS = $(AM_CXXFLAGS) $(nss_CFLAGS)
 @HAVE_HTTP_SUPPORT_TRUE@stap_httpd_CPPFLAGS = $(AM_CPPFLAGS) $(nss_CFLAGS)
@@ -513,6 +514,8 @@ clean-pkglibexecPROGRAMS:
 	../$(DEPDIR)/$(am__dirstamp)
 ../stap_httpd-privilege.$(OBJEXT): ../$(am__dirstamp) \
 	../$(DEPDIR)/$(am__dirstamp)
+../stap_httpd-cscommon.$(OBJEXT): ../$(am__dirstamp) \
+	../$(DEPDIR)/$(am__dirstamp)
 
 stap-httpd$(EXEEXT): $(stap_httpd_OBJECTS) $(stap_httpd_DEPENDENCIES) $(EXTRA_stap_httpd_DEPENDENCIES) 
 	@rm -f stap-httpd$(EXEEXT)
@@ -526,6 +529,7 @@ distclean-compile:
 	-rm -f *.tab.c
 
 @AMDEP_TRUE@@am__include@ @am__quote@../$(DEPDIR)/stap_httpd-cmdline.Po@am__quote@
+@AMDEP_TRUE@@am__include@ @am__quote@../$(DEPDIR)/stap_httpd-cscommon.Po@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@../$(DEPDIR)/stap_httpd-nsscommon.Po@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@../$(DEPDIR)/stap_httpd-privilege.Po@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@../$(DEPDIR)/stap_httpd-util.Po@am__quote@
@@ -691,6 +695,20 @@ stap_httpd-nss_funcs.obj: nss_funcs.cxx
 @AMDEP_TRUE@@am__fastdepCXX_FALSE@	$(AM_V_CXX)source='../privilege.cxx' object='../stap_httpd-privilege.obj' libtool=no @AMDEPBACKSLASH@
 @AMDEP_TRUE@@am__fastdepCXX_FALSE@	DEPDIR=$(DEPDIR) $(CXXDEPMODE) $(depcomp) @AMDEPBACKSLASH@
 @am__fastdepCXX_FALSE@	$(AM_V_CXX@am__nodep@)$(CXX) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(stap_httpd_CPPFLAGS) $(CPPFLAGS) $(stap_httpd_CXXFLAGS) $(CXXFLAGS) -c -o ../stap_httpd-privilege.obj `if test -f '../privilege.cxx'; then $(CYGPATH_W) '../privilege.cxx'; else $(CYGPATH_W) '$(srcdir)/../privilege.cxx'; fi`
+
+../stap_httpd-cscommon.o: ../cscommon.cxx
+@am__fastdepCXX_TRUE@	$(AM_V_CXX)$(CXX) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(stap_httpd_CPPFLAGS) $(CPPFLAGS) $(stap_httpd_CXXFLAGS) $(CXXFLAGS) -MT ../stap_httpd-cscommon.o -MD -MP -MF ../$(DEPDIR)/stap_httpd-cscommon.Tpo -c -o ../stap_httpd-cscommon.o `test -f '../cscommon.cxx' || echo '$(srcdir)/'`../cscommon.cxx
+@am__fastdepCXX_TRUE@	$(AM_V_at)$(am__mv) ../$(DEPDIR)/stap_httpd-cscommon.Tpo ../$(DEPDIR)/stap_httpd-cscommon.Po
+@AMDEP_TRUE@@am__fastdepCXX_FALSE@	$(AM_V_CXX)source='../cscommon.cxx' object='../stap_httpd-cscommon.o' libtool=no @AMDEPBACKSLASH@
+@AMDEP_TRUE@@am__fastdepCXX_FALSE@	DEPDIR=$(DEPDIR) $(CXXDEPMODE) $(depcomp) @AMDEPBACKSLASH@
+@am__fastdepCXX_FALSE@	$(AM_V_CXX@am__nodep@)$(CXX) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(stap_httpd_CPPFLAGS) $(CPPFLAGS) $(stap_httpd_CXXFLAGS) $(CXXFLAGS) -c -o ../stap_httpd-cscommon.o `test -f '../cscommon.cxx' || echo '$(srcdir)/'`../cscommon.cxx
+
+../stap_httpd-cscommon.obj: ../cscommon.cxx
+@am__fastdepCXX_TRUE@	$(AM_V_CXX)$(CXX) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(stap_httpd_CPPFLAGS) $(CPPFLAGS) $(stap_httpd_CXXFLAGS) $(CXXFLAGS) -MT ../stap_httpd-cscommon.obj -MD -MP -MF ../$(DEPDIR)/stap_httpd-cscommon.Tpo -c -o ../stap_httpd-cscommon.obj `if test -f '../cscommon.cxx'; then $(CYGPATH_W) '../cscommon.cxx'; else $(CYGPATH_W) '$(srcdir)/../cscommon.cxx'; fi`
+@am__fastdepCXX_TRUE@	$(AM_V_at)$(am__mv) ../$(DEPDIR)/stap_httpd-cscommon.Tpo ../$(DEPDIR)/stap_httpd-cscommon.Po
+@AMDEP_TRUE@@am__fastdepCXX_FALSE@	$(AM_V_CXX)source='../cscommon.cxx' object='../stap_httpd-cscommon.obj' libtool=no @AMDEPBACKSLASH@
+@AMDEP_TRUE@@am__fastdepCXX_FALSE@	DEPDIR=$(DEPDIR) $(CXXDEPMODE) $(depcomp) @AMDEPBACKSLASH@
+@am__fastdepCXX_FALSE@	$(AM_V_CXX@am__nodep@)$(CXX) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(stap_httpd_CPPFLAGS) $(CPPFLAGS) $(stap_httpd_CXXFLAGS) $(CXXFLAGS) -c -o ../stap_httpd-cscommon.obj `if test -f '../cscommon.cxx'; then $(CYGPATH_W) '../cscommon.cxx'; else $(CYGPATH_W) '$(srcdir)/../cscommon.cxx'; fi`
 
 # This directory's subdirectories are mostly independent; you can cd
 # into them and run 'make' without going through this Makefile.

commit 29d15c7f88c1496ee2741c6f4822d02b7885aa11

Fix finding compatible servers (broken in last commit).
* nss-server-info.cxx (nss_get_or_keep_compatible_server_info): Fix
  discarding compatible server check broken in commit eb3896e.

nss-server-info.cxx
@@ -1838,7 +1838,7 @@ nss_get_or_keep_compatible_server_info (
 
       // Check the target of the server.
       if (servers[i].sysinfo != s.kernel_release + " " + s.architecture
-	  || servers[i].sysinfo != s.architecture)
+	  && servers[i].sysinfo != s.architecture)
 	{
 	  // Target platform mismatch.
 	  servers.erase (servers.begin () + i);